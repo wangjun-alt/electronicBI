f26b5e50e967166aabfa3208f91dd65f89bb483f
wangjun-alt <2797094441@qq.com>
2023-05-31 16:26:51 +0800

    版本0.9，合同检索的实现

--- apps/contracts/urls.py
+++ apps/contracts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from apps.contracts.views import  SetContractDataView
+from apps.contracts.views import SetContractDataView, GetContractNumView, SearchContractView, GetTableDataView,UploadContractView
 
 
 
 urlpatterns = [
     # 判断用户名是否重复
     path('form-upload/', SetContractDataView.as_view()),
+    path('nums/', GetContractNumView.as_view()),
+    path('search/', SearchContractView.as_view()),
+    path('tabledata/', GetTableDataView.as_view()),
+    path('upload/', UploadContractView.as_view())
 ]
